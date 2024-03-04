c76d970266cf819903faa757634134bd42a4458f
Talha Ashfaq <talhaashfaq45@yahoo.com>
2024-03-05 01:02:50 +0500

    first assignment

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+// tsc --init
+// app.ts
+// tsc -w (means watch)
+console.log("Assalamualikum talha");
---
+++ app.ts
@@ -0,0 +1,5 @@
+// tsc --init
+// app.ts
+// tsc -w (means watch)
+
+console.log("Assalamualikum everyone");
